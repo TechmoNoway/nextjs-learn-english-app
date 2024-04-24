95e441f4329fec67a8150a81275d61051f928acc
TechmoNoway <cuty0406@outlook.com>
2024-04-24 14:13:03 +0700

    Add some UI and queries

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,53 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { getUnits, getUserProgress } from "@/db/queries";
+
+import { Header } from "./header";
+import { redirect } from "next/navigation";
+import { Unit } from "./unit";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+  const unitsData = getUnits();
+
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
+        ></UserProgress>
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title={userProgress.activeCourse.title} />
+        {units?.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={undefined}
+              activeLessonPercentage={0}
+            />
+          </div>
+        ))}
+      </FeedWrapper>
+    </div>
+  );
+};
+
+export default LearnPage;
