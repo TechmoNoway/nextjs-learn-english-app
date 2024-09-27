ca6521ccf10205b0c4ca9105742d1df121be6b8f
TechmoNoway <cuty0406@outlook.com>
2024-09-28 00:22:08 +0700

    Add payment features and shop items

--- db/schema.ts
+++ db/schema.ts
@@ -6,6 +6,7 @@ import {
   pgTable,
   serial,
   text,
+  timestamp,
 } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
@@ -32,7 +33,10 @@ export const units = pgTable("units", {
 });
 
 export const unitsRelations = relations(units, ({ many, one }) => ({
-  course: one(courses, { fields: [units.courseId], references: [courses.id] }),
+  course: one(courses, {
+    fields: [units.courseId],
+    references: [courses.id],
+  }),
   lessons: many(lessons),
 }));
 
@@ -45,10 +49,16 @@ export const lessons = pgTable("lessons", {
   order: integer("order").notNull(),
 });
 
-export const lessonsRelations = relations(lessons, ({ one, many }) => ({
-  unit: one(units, { fields: [lessons.unitId], references: [units.id] }),
-  challenges: many(challenges),
-}));
+export const lessonsRelations = relations(
+  lessons,
+  ({ one, many }) => ({
+    unit: one(units, {
+      fields: [lessons.unitId],
+      references: [units.id],
+    }),
+    challenges: many(challenges),
+  })
+);
 
 export const challengesEnum = pgEnum("type", ["SELECT", "ASSIST"]);
 
@@ -62,14 +72,17 @@ export const challenges = pgTable("challenges", {
   order: integer("order").notNull(),
 });
 
-export const challengesRelations = relations(challenges, ({ one, many }) => ({
-  lesson: one(lessons, {
-    fields: [challenges.lessonId],
-    references: [lessons.id],
-  }),
-  challengeOptions: many(challengeOptions),
-  challengeProgress: many(challengeProgress),
-}));
+export const challengesRelations = relations(
+  challenges,
+  ({ one, many }) => ({
+    lesson: one(lessons, {
+      fields: [challenges.lessonId],
+      references: [lessons.id],
+    }),
+    challengeOptions: many(challengeOptions),
+    challengeProgress: many(challengeProgress),
+  })
+);
 
 export const challengeOptions = pgTable("challenge_options", {
   id: serial("id").primaryKey(),
@@ -114,17 +127,38 @@ export const challengeProgressRelations = relations(
 export const userProgress = pgTable("user_progress", {
   userId: text("user_id").primaryKey(),
   userName: text("user_name").notNull().default("User"),
-  userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
-  activeCourseId: integer("active_course_id").references(() => courses.id, {
-    onDelete: "cascade",
-  }),
+  userImageSrc: text("user_image_src")
+    .notNull()
+    .default("/mascot.svg"),
+  activeCourseId: integer("active_course_id").references(
+    () => courses.id,
+    {
+      onDelete: "cascade",
+    }
+  ),
   hearts: integer("hearts").notNull().default(3),
   points: integer("points").notNull().default(0),
 });
 
-export const userProgressRelations = relations(userProgress, ({ one }) => ({
-  activeCourse: one(courses, {
-    fields: [userProgress.activeCourseId],
-    references: [courses.id],
-  }),
-}));
+export const userProgressRelations = relations(
+  userProgress,
+  ({ one }) => ({
+    activeCourse: one(courses, {
+      fields: [userProgress.activeCourseId],
+      references: [courses.id],
+    }),
+  })
+);
+
+export const userSubscription = pgTable("user_subscription", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull().unique(),
+  stripeCustomerId: text("stripe_customer_id").notNull().unique(),
+  stripeSubscriptionId: text("stripe_subscription_id")
+    .notNull()
+    .unique(),
+  stripePriceId: text("stripe_price_id").notNull(),
+  stripeCurrentPeriodEnd: timestamp(
+    "stripe_current_period_end"
+  ).notNull(),
+});
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,32 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/webhooks/stripe"],
 });
 
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)", "/"],
 };
+
+// import {
+//   clerkMiddleware,
+//   createRouteMatcher,
+// } from "@clerk/nextjs/server";
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/(.*)",
+//   "/api/webhooks/stripe/(.*)",
+// ]);
+
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
